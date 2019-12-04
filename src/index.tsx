9c7617a6f419ba42b36a4c7bfe15b87ed7b4fa8a
Alex Young <alex.young@hive.hr>
2019-12-04 20:22:06 +0000

    Language switching

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { I18nProvider } from "@lingui/react";
 
-const Main: React.FC = () => (
-    <I18nProvider language="en">
-        <App name="Steve" />
-    </I18nProvider>
-);
+import enCatalog from "./locales/en/messages.js";
+
+const catalogs: Record<string, any> = {
+    en: enCatalog
+};
+
+const Main: React.FC = () => {
+    const [language, setLanguage] = useState("en");
+    const [languageLoaded, setLanguageLoaded] = useState(true);
+
+    const changeLanguage = (newLanguage: string) => () => {
+        if (catalogs[newLanguage]) {
+            setLanguage(newLanguage);
+        } else {
+            setLanguageLoaded(false);
+            import(`./locales/${newLanguage}/messages.js`).then(catalog => {
+                catalogs[newLanguage] = catalog.default;
+                setLanguageLoaded(true);
+                setLanguage(newLanguage);
+            });
+        }
+    };
+
+    return (
+        <I18nProvider language={language} catalogs={catalogs}>
+            {languageLoaded ? (
+                <>Current language is {language}</>
+            ) : (
+                <>Language loading</>
+            )}
+            <br />
+            <button onClick={changeLanguage("en")}>English</button>
+            <button onClick={changeLanguage("fr")}>French</button>
+            <App name="Steve" />
+        </I18nProvider>
+    );
+};
 
 ReactDOM.render(<Main />, document.getElementById("root"));
