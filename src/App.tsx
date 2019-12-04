8572689fb8ffe6708a37b7635c8877c71db05a37
Alex Young <alex.young@hive.hr>
2019-12-04 19:55:12 +0000

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Trans, Plural } from "@lingui/macro";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC<{ name: string }> = ({ name }) => {
+    const count = 0;
+
+    return (
+        <div>
+            <Trans>Hello my name is Alex!</Trans>
+            <br />
+            <Trans>Hello my name is {name}</Trans>
+            <br />
+            <Plural
+                value={count}
+                _0="Boo, no apples"
+                one="I have # apple"
+                other="I have # apples"
+            />
+        </div>
+    );
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { I18nProvider } from "@lingui/react";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Main: React.FC = () => (
+    <I18nProvider language="en">
+        <App name="Steve" />
+    </I18nProvider>
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Main />, document.getElementById("root"));
